8b687c66f9dbb0e506db1f98b63101df0f016575
BelzDaniel <BelzDaniel6@gmail.com>
2024-09-06 00:28:55 +0300

    fix: not finished! finish the job as soon ae

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import app_logo from "../../assets/app_logo.png";
+import Os from "@/assets/Os.jpg";
+
 import {
   Carousel,
   CarouselApi,
@@ -8,6 +10,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function Home() {
   const [api, setApi] = React.useState<CarouselApi>();
@@ -29,16 +32,23 @@ export default function Home() {
 
   return (
     <div className="w-full h-full p-12 flex justify-around items-center">
-      <div className="w-80">
-        <Carousel setApi={setApi}>
-          <CarouselContent>
-            <CarouselItem>
+      <div className="w-96">
+        <Carousel
+          className="w-full"
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+          setApi={setApi}>
+          <CarouselContent className="w-85">
+            <CarouselItem className="">
+              <img src={Os} />
+            </CarouselItem>
+            <CarouselItem className="">
               <img src={app_logo} />
             </CarouselItem>
-            <CarouselItem>
-              <img src={app_logo} />
-            </CarouselItem>
-            <CarouselItem>
+            <CarouselItem className="">
               <img src={app_logo} />
             </CarouselItem>
           </CarouselContent>
@@ -48,6 +58,12 @@ export default function Home() {
         <div className="py-2 text-center text-sm text-muted-foreground">
           {current} of {count}
         </div>
+      </div>
+      <div>
+        <span className="flex justify-center w-4">
+          גדששגדדדדדדדדדדדדדדדדדדדדדדדדדדדדדדדדגג גג לגגלמלמג גלחגלחגלגח אנחנו
+          צוות פמפמםם ואנחנו רוצים שלום בינינו. .כל הבקשות שלכם, במקום אחד
+        </span>
       </div>
       <div>
         <img className="w-48" src={app_logo} />
