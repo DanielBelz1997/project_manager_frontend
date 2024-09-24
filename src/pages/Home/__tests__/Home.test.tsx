9822f84ffc9b67f3a5efd62e7550337da11093a1
BelzDaniel <BelzDaniel6@gmail.com>
2024-09-24 22:37:11 +0300

    removed testing

--- src/pages/Home/__tests__/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../Home";
-import { describe, it, expect, vi } from "vitest";
-
-vi.mock("src/components/ui/carousel.tsx", () => ({
-  Carousel: ({ children }: React.PropsWithChildren<object>) => (
-    <div>{children}</div>
-  ),
-}));
