f9ee20aa02fbd7438c81e12cc13aa73b53ed5c3f
BelzDaniel <BelzDaniel6@gmail.com>
2024-11-29 15:13:54 +0200

    feat: starting to see somthing

--- src/pages/Admin/AdminHomePage.tsx
+++ src/pages/Admin/AdminHomePage.tsx
@@ -1,7 +1,29 @@
+import { Table } from "lucide-react";
+import GridLayout from "react-grid-layout";
+
 export const AdminHomePage = () => {
+  const layout = [
+    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
+    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: "c", x: 4, y: 0, w: 1, h: 2 },
+  ];
   return (
     <div className="w-full h-full p-40 flex justify-around items-center">
-      help
+      <GridLayout
+        className="layout"
+        layout={layout}
+        cols={12}
+        rowHeight={30}
+        width={1200}>
+        <div key="a">a</div>
+        <div className="bg-slate-300" key="b">
+          <Table />
+        </div>
+        <div key="c">c</div>
+      </GridLayout>
     </div>
   );
 };
+
+// className="w-full h-full p-40 flex justify-around items-center"
+
