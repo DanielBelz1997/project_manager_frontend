c7e1e9d1c8570c040051e3c26adfbbf0cfa3ef3a
BelzDaniel <BelzDaniel6@gmail.com>
2024-12-10 21:54:29 +0200

    fdsfdsfds

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
-//fd two
+//fd tworerewrewgfdgdfgfdgdfgdf
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
