6e729baf425bfab14843e97bd45992a0ee37a49e
BelzDaniel <BelzDaniel6@gmail.com>
2024-12-13 11:17:24 +0200

    test: test to create Contact

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -2,26 +2,20 @@ import { AuthState } from "@/types/auth";
 import { create } from "zustand";
 
 export const useAuthStore = create<AuthState>((set) => ({
-  token: localStorage.getItem("JWT") || null,
-  username: localStorage.getItem("username") || null,
-  role: localStorage.getItem("role") || null,
+  token: null,
+  username: null,
+  role: null,
   setUsername: (username: string) => {
-    localStorage.setItem("username", username);
     set({ username });
   },
   setToken: (token: string) => {
-    localStorage.setItem("JWT", token);
     set({ token });
   },
   setRole: (role: string) => {
-    localStorage.setItem("role", role);
     set({ role });
   },
   clearLogin: () => {
-    localStorage.removeItem("JWT");
-    localStorage.removeItem("username");
-    localStorage.removeItem("role");
-    set({ token: null });
+    set({ token: null, username: null, role: null });
   },
 }));
 
